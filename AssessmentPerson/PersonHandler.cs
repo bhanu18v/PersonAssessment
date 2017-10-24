9fb100aa55593e1a5ffbe8736632094f90c63401
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-10-24 13:03:47 +0200

    Added Personhandler & Code in Program

---
+++ AssessmentPerson/PersonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentPerson
+{
+    class PersonHandler
+    {
+        public void SetAge (Person pers, int age)
+        {
+            pers.Age = age;
+        }
+        public Person CreatePerson (int iAge, string fName,string lName, float Height, float Weight)
+        {
+            return;
+        }
+    }
+   
+    
+}
--- AssessmentPerson/Program.cs
+++ AssessmentPerson/Program.cs
@@ -10,11 +10,37 @@ namespace AssessmentPerson
     {
         static void Main(string[] args)
         {
-            Person pers = new Person();
-            pers.Age = 20;
-            pers.Fname = "kUNAL";
-            Console.WriteLine("enter the persons name");
 
+            Person person1 = new Person();
+            Console.WriteLine("Enter your First Name:");
+            person1.Fname= Console.ReadLine();
+
+            Console.WriteLine("Enter your Last Name:");
+            person1.Lname = Console.ReadLine();
+
+            Console.WriteLine("Hi"+" "+person1.Fname + " "+person1.Lname);
+
+            Console.WriteLine("Enter your age:");
+            Person pAge = new Person();
+            PersonHandler newage = new PersonHandler();
+            newage.SetAge(pAge, 25);
+            Console.WriteLine(pAge.Age);
+            //string sAge = Console.ReadLine();
+            //int iAge = person1.Age;
+            //if(!int.TryParse(sAge, out iAge))
+            //{
+            //    Console.WriteLine("Invalid Age");
+            //}
+
+            Console.WriteLine("How tall are you:");
+            person1.Height =float.Parse( Console.ReadLine());
+
+            Console.WriteLine("How much do you weigh:");
+            person1.Weight = float.Parse(Console.ReadLine());
+
+
+           
+            
         }
     }
 }

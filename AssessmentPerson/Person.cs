566010169cecaf4b0ffa57f0ab12dfec13eb13b4
bhanu18v <bhanu18v@gmail.com>
2017-10-24 10:35:30 +0200

    AddedPerson

---
+++ AssessmentPerson/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentPerson
+{
+    class Person
+    {
+        private string name = "N/A";
+        private int age = 0;
+
+        // Declare a Name property of type string:
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        // Declare an Age property of type int:
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name = " + Name + ", Age = " + Age;
+        }
+
+
+    }
+}
--- AssessmentPerson/Program.cs
+++ AssessmentPerson/Program.cs
@@ -11,6 +11,35 @@ namespace AssessmentPerson
         static void Main(string[] args)
         {
             Console.WriteLine("hi");
+
+            // var e1 = new Employee("Jane");
+            //e1.Salary = 25000;
+            //var e2 = new Employee("Anne");
+            //e2.Salary = 23000;
+
+            //var person = new Person();
+            //person.age = 22;
+            //Console.WriteLine("Person Details are - {0}" , person);
+            //Console.ReadLine();
+
+            Person person = new Person();
+
+            // Print out the name and the age associated with the person:
+            Console.WriteLine("Person details - {0}", person);
+
+            // Set some values on the person object:
+            person.Name = "Joe";
+            person.Age = 99;
+            Console.WriteLine("Person details - {0}", person);
+
+            // Increment the Age property:
+            person.Age += 1;
+            Console.WriteLine("Person details - {0}", person);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }

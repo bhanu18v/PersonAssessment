92222ee09a848abfa3489f796daa20ae048befc7
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-10-24 16:15:28 +0200

    Removed all codes in classes

--- AssessmentPerson/PersonHandler.cs
+++ AssessmentPerson/PersonHandler.cs
@@ -23,5 +23,34 @@ namespace AssessmentPerson
             OnePer.Weight = Weight;
             return OnePer;
         }
+        public bool check()
+        {
+            Console.WriteLine("Enter your First Name:");
+            string sName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name:");
+            string lName = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            int intAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("How tall are you:");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("How much do you weigh:");
+            float weight = float.Parse(Console.ReadLine() + "\n" + "\n");
+            Person AddPrson = new Person();
+            PersonHandler newPerson1 = new PersonHandler();
+            AddPrson = newPerson1.CreatePerson(intAge, sName, lName, height, weight);
+
+
+            //Printing the output
+
+            Console.WriteLine("=================================================================");
+
+            Console.Write("Hi" + " " + sName + " " + lName + "\n");
+            Console.Write("Your age is : " + intAge + "\n");
+            Console.Write("Your Weight is : " + weight + "\n");
+            Console.Write("Your Height is  : " + height + "\n");
+
+            Console.ReadLine();
+            return true;
+        }
     }
 }
--- AssessmentPerson/Program.cs
+++ AssessmentPerson/Program.cs
@@ -11,32 +11,9 @@ namespace AssessmentPerson
         static void Main(string[] args)
         {
 
-            
-            Console.WriteLine("Enter your First Name:");
-            string sName = Console.ReadLine();
-            Console.WriteLine("Enter your Last Name:");
-            string lName = Console.ReadLine();
-            Console.WriteLine("Enter your age:");
-            int intAge = int.Parse(Console.ReadLine());
-            Console.WriteLine("How tall are you:");
-            float height =float.Parse(Console.ReadLine());
-            Console.WriteLine("How much do you weigh:");
-            float weight = float.Parse(Console.ReadLine() + "\n" + "\n");
-            Person AddPrson = new Person();
-            PersonHandler newPerson1 = new PersonHandler();
-            AddPrson = newPerson1.CreatePerson(intAge, sName, lName, height, weight);
 
-
-            //Printing the output
-
-            Console.WriteLine("=================================================================");
-
-            Console.Write("Hi" + " " + sName + " " + lName + "\n");
-            Console.Write("Your age is : " + intAge + "\n");
-            Console.Write("Your Weight is : " + weight + "\n");
-            Console.Write("Your Height is  : " + height + "\n");
-           
-            Console.ReadLine();
+            PersonHandler newclass = new PersonHandler();
+            newclass.check();
 
             //OLD CODE
             //Person person1 = new Person();

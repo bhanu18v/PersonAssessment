eeb01437c196548190c25cf3448cfc5097291e8c
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-10-26 13:14:48 +0200

    Changes in Inheritance

---
+++ AssessmentPerson/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentPerson
+{
+    public class Dog:Animal
+    {
+        private string Color;
+
+        public string color
+        {
+            get { return Color; }
+            set { Color = value; }
+        }
+
+        public Dog ()
+        {
+
+        }
+
+        public Dog(string name,int weight, int age, string color):base (name,weight,age)
+        {
+            Color = color;
+        }
+    }
+}
---
+++ AssessmentPerson/Horse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentPerson
+{
+    public class Horse:Animal
+    {
+        public int height
+        {
+            get { return Height; }
+            set { Height = value; }
+        }
+        private int Height;
+
+        public Horse()
+        {
+
+        }
+
+        public Horse(string name,int weight,int age,int height): base(name,weight,age)
+        {
+                  Height = height;
+         }
+    }
+}

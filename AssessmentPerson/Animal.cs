636afec9ea91a6234871018fa57b341aadedf9f1
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-10-26 13:12:35 +0200

    Inhertance done

--- AssessmentPerson/Animal.cs
+++ AssessmentPerson/Animal.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace AssessmentPerson
 {
-    class Animal
+   public class Animal
+
+    {    
+        private string  Name;
+        private int Weight;
+        private int Age;
+
+        public string  name {get { return Name; } set { Name = value; }}
+        public int weigth{get { return Weight; } set { Weight = value; }}
+        public int age {get { return Age; }set { Age = value; }}
+
+
+        public Animal()
         {
 
-        public string Name { get; set; }
+        }
+        public Animal(string name,int weight,int age)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
 
+        }
     }
+   
 }
